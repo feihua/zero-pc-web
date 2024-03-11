f085205b6c75e6cd78c0b3c7d3600706af673ba8
koobe <koobe.liu@uaf.com.cn>
2024-03-11 10:12:08 +0800

    集成scss样式 1.npm i node-sass -D

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button} from 'antd';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+    <>
+        <div className={"one"}>
+            <div className={"two"}></div>
+        </div>
+        <div className="App">
+            <Button type="primary">Button</Button>
+        </div>
+    </>
+);
 
 export default App;
