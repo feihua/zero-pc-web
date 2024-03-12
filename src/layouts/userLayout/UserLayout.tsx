eb8548f1285d067eff529a09079c8e713f1e67f7
koobe <koobe.liu@uaf.com.cn>
2024-03-12 14:04:10 +0800

    用户登录和注册公共布局

---
+++ src/layouts/userLayout/UserLayout.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./UserLayout.module.css";
+import logo from "../../assets/logo.svg";
+
+interface UserLayoutProps {
+    children: React.JSX.Element
+}
+
+export const UserLayout: React.FC<UserLayoutProps> = (props) => {
+
+    return <>
+        <div className={styles.pageContent}>
+
+            <div className={styles.loginTop}>
+                <div className={styles.loginTopInner}>
+                    <img src={logo} className={"logo"} alt="logo"/>
+                    <h1 className={styles.title}>React 购物网</h1>
+                </div>
+                <text className={styles.desc}>zero mall 电商项目pc-web版本</text>
+            </div>
+            <div className={styles.childrenForm}>
+                {props.children}
+            </div>
+        </div>
+    </>
+}
+
